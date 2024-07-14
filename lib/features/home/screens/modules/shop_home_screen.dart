b341d0bc6210b19c3fcafd4241754d1bc8f9f729
Mohammed El Shora <riyadm2001@gmail.com>
2024-07-14 18:49:57 +0300

    select custom features in app to work

--- lib/features/home/screens/modules/shop_home_screen.dart
+++ lib/features/home/screens/modules/shop_home_screen.dart
@@ -1,23 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:sixam_mart/features/home/widgets/brands_view_widget.dart';
 import 'package:sixam_mart/helper/auth_helper.dart';
 import 'package:sixam_mart/util/images.dart';
 import 'package:sixam_mart/features/flash_sale/widgets/flash_sale_view_widget.dart';
 import 'package:sixam_mart/features/home/widgets/bad_weather_widget.dart';
-import 'package:sixam_mart/features/home/widgets/views/product_with_categories_view.dart';
-import 'package:sixam_mart/features/home/widgets/views/featured_categories_view.dart';
-import 'package:sixam_mart/features/home/widgets/views/popular_store_view.dart';
-import 'package:sixam_mart/features/home/widgets/views/item_that_you_love_view.dart';
 import 'package:sixam_mart/features/home/widgets/views/just_for_you_view.dart';
 import 'package:sixam_mart/features/home/widgets/views/most_popular_item_view.dart';
-import 'package:sixam_mart/features/home/widgets/views/new_on_mart_view.dart';
-import 'package:sixam_mart/features/home/widgets/views/middle_section_banner_view.dart';
-import 'package:sixam_mart/features/home/widgets/views/special_offer_view.dart';
 import 'package:sixam_mart/features/home/widgets/views/visit_again_view.dart';
 import 'package:sixam_mart/features/home/widgets/banner_view.dart';
 
 import '../../widgets/views/category_view_grid.dart';
-import '../../widgets/views/store_wise_banner_view.dart';
 
 class ShopHomeScreen extends StatelessWidget {
   const ShopHomeScreen({super.key});
@@ -49,17 +40,17 @@ class ShopHomeScreen extends StatelessWidget {
       const CategoryViewGrid(),
       isLoggedIn ? const VisitAgainView() : const SizedBox(),
       const FlashSaleViewWidget(),
-      const MostPopularItemView(isFood: false, isShop: true), // * Incorrect use of ParentDataWidget.
-      const MiddleSectionBannerView(),
-      const PopularStoreView(),
-      const BrandsViewWidget(),
-      const SpecialOfferView(isFood: false, isShop: true),
-      const ProductWithCategoriesView(fromShop: true),
       const JustForYouView(),
-      const FeaturedCategoriesView(),
+      const MostPopularItemView(isFood: false, isShop: true),
+      // const MiddleSectionBannerView(),
+      // const PopularStoreView(),
+      // const BrandsViewWidget(),
+      // const SpecialOfferView(isFood: false, isShop: true),
+      // const ProductWithCategoriesView(fromShop: true),
+      // const FeaturedCategoriesView(),
       // const StoreWiseBannerView(),
-      const ItemThatYouLoveView(forShop: true,),
-      const NewOnMartView(isShop: true,isPharmacy: false),
+      // const ItemThatYouLoveView(forShop: true,),
+      // const NewOnMartView(isShop: true,isPharmacy: false),
       // const PromotionalBannerView(),
     ]);
   }
